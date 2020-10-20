6a3b99ebde406a09a381ec4d66eb39191c2842b9
Christophe Van Neste <christophe.vanneste@ugent.be>
2020-10-20 17:32:08 +0200

    install doc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='apetype',
-      version='0.0.5-dev0',
+      version='0.0.5',
       description='Embracing builtin python modules argparse and typing for pipelines',
       url='https://github.com/dicaso/apetype',
       author='Christophe Van Neste',
